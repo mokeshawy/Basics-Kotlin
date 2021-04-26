78bec10ec227016ed688f11d97a5dbf5c58a0126
mohamed <moahmed_gsm@msn.com>
2021-04-26 14:53:58 +0200

    All Basics kotlin language will upload hear

---
+++ src/DataTypes.kt
@@ -0,0 +1,21 @@
+fun main(args : Array<String>){
+    
+    var name = "Mohamed Keshawey"
+    println(name)
+
+    val age = 28
+    println(age)
+
+    var developer = "Developer"
+    println(developer)
+
+    var avg = 2.3
+    println(avg)
+
+    var title : String? = null
+    print(title)
+
+    title = "mohamed"
+    println(title)
+
+}
---
+++ src/Kotlin.kt
@@ -0,0 +1,26 @@
+fun main(args : Array<String>){
+
+    // val name = "Mohamed Eid" // val :- variable do nt change
+//
+//    var name = "Mohamed Eid" // when using var this variable can be change
+//    println(name)
+
+    var name = "Mohamed Keshawey"
+    println(name)
+
+    val age = 28
+    println(age)
+
+    var developer = "Developer"
+    println(developer)
+
+    var avg = 2.3
+    println(avg)
+
+    var title : String? = null
+    print(title)
+
+    title = "mohamed"
+    println(title)
+
+}
